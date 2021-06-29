af1f4ce976e53b305341bc60a77dc702a6768a84
Lissett Carrero <katliss.26@gmail.com>
2021-06-28 21:43:13 -0400

    Primer commit

---
+++ lib/widgets/scan_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+class ScanButton extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () async{
+        //String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#3D8BEF', 'Cancelar', false, ScanMode.QR);
+        String barcodeScanRes = 'https://www.google.com/';
+        print(barcodeScanRes);
+      }, 
+      child: Icon(Icons.filter_center_focus),
+      elevation: 0,
+    );
+  }
+}
